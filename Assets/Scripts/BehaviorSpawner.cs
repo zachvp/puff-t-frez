2ade6fac548171cf8f8ce8701376a1072c363537
Zach Vega-Perkins <vegaperk@gmail.com>
2018-03-23 18:29:24 -0700

    add BehaviorSpawner object to create core scripts automatically

---
+++ Assets/Scripts/BehaviorSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Responsible for automatically creating all the core dependencies that need to be present in the scene. This should
+/// Be configured to run before any other scripts.
+/// </summary>
+public class BehaviorSpawner : MonoBehaviour {
+	[SerializeField] private MonoBehaviour[] behaviors;
+
+	public void Awake() {
+		// Spawn all behaviors
+		foreach (MonoBehaviour behavior in behaviors) {
+			var instance = Instantiate (behavior) as MonoBehaviour;
+			instance.transform.SetParent (transform);
+		}
+	}
+}
