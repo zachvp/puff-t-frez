a88c879885373a80cd0d77be0389a758dbc02f7c
Zach Vega-Perkins <zachvp@Zachs-MacBook-Pro.local>
2018-03-23 17:05:03 -0700

    initial commit of prototype - basic directional movement and jump working

---
+++ Assets/Scripts/CoreDebug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+public class CoreDebug {
+	public static void LogCollection(String name, System.Object blob) {
+		var logString = new StringBuilder ();
+		var i = 0;
+		var collection = (ICollection) blob;
+
+		logString.AppendFormat ("---Begin collection: {0}", name);
+
+		foreach (System.Object element in collection) {
+			if (element is ICollection) {
+				LogCollection (name, element);
+			} else {
+				logString.AppendFormat ("\nelement {0}: {1}", i, element.ToString ());
+			}
+			i++;
+		}
+
+		logString.Append ("---End collection");
+		Debug.LogFormat ("{0}", logString.ToString ());
+	}
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerMotor : MonoBehaviour
+{
+	[SerializeField]
+	private CharacterController2D controller;
+
+	public AnimationCurve accelerationCurve;
+
+	private Vector2 velocity;
+
+	private Vector2 controlDirection;
+
+	private Stack<Vector2> controlDirections;
+
+	public void Awake()
+	{
+		controller = GetComponent<CharacterController2D> ();
+		controlDirections = new Stack<Vector2> ();
+	}
+		
+
+	public void Update()
+	{
+		// Horizontal control
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			InputRight ();
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			InputLeft ();
+		}
+
+		// Vertical control
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			InputUp ();
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			InputDown ();
+		}
+
+		Debug.LogFormat ("Control direction: {0}", controlDirection);
+
+		Boolean isNoHorizontalInput = !Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow);
+		Boolean isConcurrentHorizontalInput = Input.GetKey (KeyCode.RightArrow) && Input.GetKey (KeyCode.LeftArrow);
+
+		if (isNoHorizontalInput || isConcurrentHorizontalInput)
+		{
+			InputHorizontalNone ();
+		}
+		if ((!Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.DownArrow)))
+		{
+			InputVerticalNone ();
+		}
+
+		controlDirections.Push (controlDirection);
+
+		Debug.Assert (controlDirection.magnitude <= Mathf.Sqrt (2), "Magnitude of control direction exceeded maximum");
+//		CoreDebug.LogCollection ("ControlDirections", controlDirections);
+
+		if (controller.isGrounded) {
+			// Horizontal movement
+			if (Mathf.Abs(velocity.x) < 220)
+			{
+				velocity.x += controlDirection.x * 60;
+			}
+
+			// Horizontal deceleration
+			if (Mathf.Abs (velocity.x) < 25) {
+				velocity.x = 0;
+			} else {
+				velocity.x -= 30 * Mathf.Sign (velocity.x);
+			}
+
+			if (controlDirection.y > 0) {
+				// TODO: Move to ApplyJump function
+				velocity.y = 1200;
+				velocity.x += controlDirection.x * 160;
+			}
+		} else {
+			// Air directional influence
+			if (Mathf.Abs(velocity.x) < 220)
+			{
+				velocity.x += controlDirection.x * 60;
+				if (isHorizontalDirectionChanged ()) {
+					velocity.x += controlDirection.x * 120;
+				}
+//				Debug.LogFormat ("Apply air directional influence. Velocity: {0}", velocity);
+			}
+
+			// Apply gravity
+			velocity.y -= 60; // TODO: Make this a constant
+		}
+
+		// Apply limits
+		if (controller.isGrounded) {
+			velocity.x = Mathf.Clamp (velocity.x, -500, 500);
+		} else {
+			velocity.x = Mathf.Clamp (velocity.x, -2000, 2000);
+		}
+
+		controller.move(Time.deltaTime * velocity);
+		velocity = controller.velocity;
+
+//		Debug.LogFormat ("Velocity: {0}", velocity);
+	}
+
+	// Input functions
+	public void InputRight () {
+		controlDirection.x = Mathf.Clamp (controlDirection.x + 1, 0, 1);
+	}
+
+	public void InputLeft () {
+		controlDirection.x = Mathf.Clamp (controlDirection.x - 1, -1, 0);	
+	}
+
+	public void InputUp () {
+		controlDirection.y = Mathf.Clamp (controlDirection.y + 1, 0, 1);
+	}
+
+	public void InputDown () {
+		controlDirection.y = Mathf.Clamp (controlDirection.y - 1, -1, 1);
+	}
+
+	public void InputHorizontalNone () {
+		controlDirection.x = 0;
+	}
+
+	public void InputVerticalNone () {
+		controlDirection.y = 0;
+	}
+
+	private Boolean isHorizontalDirectionChanged () {
+		if (controlDirections.Count > 1) {
+			var controlStateCurrent = controlDirections.Pop ();
+			var controlStatePrevious = controlDirections.Pop ();
+
+			controlDirections.Push (controlStatePrevious);
+			controlDirections.Push (controlStateCurrent);
+
+			return controlStateCurrent.x != controlStatePrevious.x;
+		}
+
+		return false;
+	}
+}
