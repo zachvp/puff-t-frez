111b3501a9523807ce2061ba0e932c830ed498f9
Zach Vega-Perkins <vegaperk@gmail.com>
2018-04-02 09:48:38 -0700

    add x flip check to input buffer

--- Assets/Scripts/InputBuffer.cs
+++ Assets/Scripts/InputBuffer.cs
@@ -21,10 +21,25 @@ public class InputBuffer {
 		return IsInputReleased ((int) checkInputDirection.x, inputBuffer.Count, frameWindow);
 	}
 
+	/// <summary>
+	/// Determines whether the given inputMagnitude has flipped from previous frames' magnitude.
+	/// </summary>
+	public bool IsInputXFlipped(int inputMagnitude, int frameWindow) {
+		var window = GetFrameWindow (frameWindow);
+		var isFlipped = false;
+
+		foreach (Vector2 input in window) {
+			if (Mathf.Abs(input.x) > 0 && (int) input.x == -inputMagnitude) {
+				isFlipped = true;
+				break;
+			}
+		}
+
+		return isFlipped;
+	}
+
 	private bool IsInputReleased(int inputMagnitude, int bufferCount, int frameWindow) {
-		var startIndex = Mathf.Max (0, bufferCount - (1 + frameWindow));
-		var windowLength = Mathf.Min (frameWindow, bufferCount);
-		var window = inputBuffer.GetRange (startIndex, windowLength);
+		var window = GetFrameWindow (frameWindow);
 		var isReleased = false;
 
 		// Iterate through the window, checking if given input direction was pressed, then released.
@@ -36,5 +51,18 @@ public class InputBuffer {
 		}
 
 		return isReleased;
+	}
+
+	/// <summary>
+	/// Returns a list of the most recent frames with a given window size.
+	/// </summary>
+	/// <returns>The frame window.</returns>
+	/// <param name="frameWindow">Frame window.</param>
+	private List<Vector2> GetFrameWindow(int frameWindow) {
+		var startIndex = Mathf.Max (0, inputBuffer.Count - (1 + frameWindow));
+		var windowLength = Mathf.Min (frameWindow, inputBuffer.Count);
+		var window = inputBuffer.GetRange (startIndex, windowLength);
+
+		return window;
 	}
 }
