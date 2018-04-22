a6969ccfe696d63d8db743c59473a077badaf7e7
Zach Vega-Perkins <vegaperk@gmail.com>
2018-04-21 22:24:06 -0700

    add event helper class

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,21 @@
+﻿public delegate void EventHandler();
+public delegate void EventHandler<T>(T args);
+
+public static class Events {
+    public static void RaiseEvent(EventHandler eventHandler) {
+        // Temp variable for thread safety.
+        var threadsafeHandler = eventHandler;
+        if (threadsafeHandler != null) {
+            threadsafeHandler();
+        }
+    }
+
+    public static void RaiseEvent<T>(EventHandler<T> eventHandler, T args) {
+        // Temp variable for thread safety.
+        var threadsafeHandler = eventHandler;
+        if (threadsafeHandler != null)
+        {
+            threadsafeHandler(args);
+        }
+    }
+}
