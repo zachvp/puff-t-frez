c08c55eba1c26cedaa0c6619267ed29ba8abb0c4
Zach Vega-Perkins <vegaperk@gmail.com>
2018-04-21 22:24:27 -0700

    add initializer for player character

---
+++ Assets/Scripts/PlayerCharacterInitializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCharacterInitializer : MonoBehaviour {
+    public EventHandler<PlayerCharacterInitializer> OnCreate;
+
+    public InputBuffer inputBuffer { get; private set; }
+
+    public void Awake() {
+        inputBuffer = new InputBuffer();
+	}
+
+	public void Start() {
+        // Raise the create event in start so every entity that needs
+        // a reference to this can subscribe to it in Awake().
+        Events.RaiseEvent(OnCreate, this);
+	}
+}
