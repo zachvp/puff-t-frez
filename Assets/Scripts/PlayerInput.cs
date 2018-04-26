6fa61d1378da6f782495fd2ca564d091ccca1a1f
Zach Vega-Perkins <vegaperk@gmail.com>
2018-04-25 18:40:19 -0700

    create player input data structures

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// Represents a snapshot of input in a single frame.
+public class PlayerInputSnapshot {
+    public PlayerInput pressedInput;
+    public PlayerInput releasedInput;
+}
+
+public class PlayerInput {
+    // Pressed states
+    public Vector2 movement;
+    public bool jump;
+}
