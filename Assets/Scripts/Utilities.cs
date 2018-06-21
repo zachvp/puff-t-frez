1646acb3667548df3b75e54e9a9ac1a850669b89
Zach Vega-Perkins <vegaperk@gmail.com>
2018-06-20 21:35:23 -0700

    add utils class

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class StoreTransform {
+	public Vector3 position;
+	public Vector3 scale;
+	public Quaternion rotation;
+
+	public StoreTransform() {}
+
+	public StoreTransform(Transform t) {
+		position = t.position;
+		scale = t.localScale;
+		rotation = t.rotation;
+	}
+}
