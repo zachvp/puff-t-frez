b75a1e19cf72e916bfd4310167c671b30cf35ff5
Zach Vega-Perkins <vegaperk@gmail.com>
2018-06-20 22:04:59 -0700

    move remainder of update logic to parent player input ctrl class

--- Assets/ScriptableObjects/PlayerMotorData.cs
+++ Assets/ScriptableObjects/PlayerMotorData.cs
@@ -6,8 +6,8 @@ public class PlayerMotorData : ScriptableObject {
 	public int velocityHorizontalAirMax = 2000;
 	public int accelerationHorizontalAir = 200;
 	public int jumpCountMax = 1;
-	public int velocityJumpImpulse = 800;
-	public int velocityJumpAdditive = 300;
+	public int velocityJumpImpulse = 400;
+	public int velocityJumpAdditive = 280;
 	public int velocityJumpMax = 4800;
     public int velocityWallJumpHorizontal = 400;
     public int velocityWallJumpVertical = 3600;
@@ -16,6 +16,6 @@ public class PlayerMotorData : ScriptableObject {
 	/// <summary>
 	/// How many frames the additive jump will last.
 	/// </summary>
-	public int frameLimitJumpAdditive = 12;
+	public int frameLimitJumpAdditive = 24;
 	public int frameLimitJumpGravityImmunity = 2;
 }
