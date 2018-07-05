f46a572fab3412f85ace8fad9a683969d9d5f21d
Zach Vega-Perkins <vegaperk@gmail.com>
2018-07-04 18:30:12 -0600

    set up marionette - last commit before things break temporarily

--- Assets/Scripts/PlayerMarionette.cs
+++ Assets/Scripts/PlayerMarionette.cs
@@ -3,9 +3,11 @@
 // Responsible for
 //    Passing input to limbs
 //    Enabling/disabling limbs
-public class PlayerMarionette {
+public class PlayerMarionette : IPlayerMarionette {
 	private IPlayerInput playerInput;
+	private ILobInput handGrenadeInput;
 
+	private Entity handGrenadeTemplate;
     
 	// TODO: Remove plan
     // Player character init
@@ -14,14 +16,27 @@ public class PlayerMarionette {
     //        limb 1
     //        ....
 
-	public PlayerMarionette(IPlayerInput inPlayerInput) {
+	public PlayerMarionette(IPlayerInput inPlayerInput, ILobInput inHandGrenadeInput) {
 		playerInput = inPlayerInput;
+		handGrenadeInput = inHandGrenadeInput;
 	}
 
+	public void ApplyPlayerInput(PlayerInputSnapshot snapshot) {
+		playerInput.ApplyInput(snapshot);
+	}
 
+	public void ApplyHandInput() {
+		handGrenadeInput.Lob();
+	}
+
+	public void ApplyDeltaTime(float deltaTime) {
+		playerInput.ApplyDeltaTime(deltaTime);
+	}
 }
 
 public interface IPlayerMarionette {
 	void ApplyPlayerInput(PlayerInputSnapshot snapshot);
+	void ApplyHandInput();
+
 	void ApplyDeltaTime(float deltaTime);
 }
