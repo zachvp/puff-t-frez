fa7e59a143cf20270eb7a09172cc3c303f09a0f4
Zach Vega-Perkins <vegaperk@gmail.com>
2018-07-23 08:00:29 -0700

    small name changes

--- Assets/Scripts/CoreUtilities.cs
+++ Assets/Scripts/CoreUtilities.cs
@@ -4,35 +4,35 @@ public static class CoreUtilities
 {
 	public static Vector3 NormalizePosition(Vector3 position)
 	{
-		var prime = Vector3.zero;
+		var r = Vector3.zero;
 
-		prime.x = Mathf.RoundToInt(position.x);
-		prime.y = Mathf.RoundToInt(position.y);
-		prime.z = Mathf.RoundToInt(position.z);
+		r.x = Mathf.RoundToInt(position.x);
+		r.y = Mathf.RoundToInt(position.y);
+		r.z = Mathf.RoundToInt(position.z);
 
-		return prime;
+		return r;
 	}
 
 	public static Vector2 NormalizeScale(Vector3 scale)
 	{
-		var result = Vector3.zero;
+		var r = Vector3.zero;
 		var digits = 3;
 		var power = 10 * digits;
 
-		result.x = Mathf.Round(scale.x * power) / power;
-		result.y = Mathf.Round(scale.y * power) / power;
-		result.y = Mathf.Round(scale.z * power) / power;
+		r.x = Mathf.Round(scale.x * power) / power;
+		r.y = Mathf.Round(scale.y * power) / power;
+		r.y = Mathf.Round(scale.z * power) / power;
 
-		return result;
+		return r;
 	}
 
 	public static Vector3 GetWorldSpaceSize(Vector2 bounds, BoxCollider2D collider, float multiplier)
     {
-        var result = Vector3.zero;
+		var r = Vector3.zero;
 
-        result.x = bounds.x * collider.size.x;
-        result.y = bounds.y * collider.size.y;
+        r.x = bounds.x * collider.size.x;
+        r.y = bounds.y * collider.size.y;
 
-        return result * multiplier;
+        return r * multiplier;
     }
 }
