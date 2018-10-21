78d8ee8bafa0f9bc114c1e4dc44d0e12681297b9
zach vp <vegaperk@gmail.com>
2018-10-21 11:21:04 -0700

    add dpad support

--- Assets/Scripts/PlayerInputControllerGamepad.cs
+++ Assets/Scripts/PlayerInputControllerGamepad.cs
@@ -17,11 +17,13 @@ public class PlayerInputControllerGamepad : InputController<PlayerInput, PlayerM
             var device = InputManager.Devices[0];
 			var crouchThreshold = 0.7f;
 			var leftStick = device.LeftStick.Value;
+            var dPad = device.DPad.Value;
 
 			// TODO: Put in utility class
 			var threshold = 0.2f;
 			var newDir = input.direction.Vector;
 
+            // Analog stick directions
 			if (Mathf.Abs(leftStick.x) > threshold)
 			{
 				newDir.x = leftStick.x;
@@ -31,10 +33,13 @@ public class PlayerInputControllerGamepad : InputController<PlayerInput, PlayerM
 				newDir.y = leftStick.y;
 			}
 
+            // DPad directions
+            newDir = dPad;
+
 			input.direction.Update(newDir);
 			input.direction.CardinalizeVector();
 
-			input.crouch = leftStick.y < -crouchThreshold;
+			input.crouch = leftStick.y < -crouchThreshold || dPad.y < 0;
 			input.jump = device.Action1.IsPressed;
         }
     }
